d176bb440e383bfb157c4319397c9f5fcec42e5c
shreyash19-pixel <shreyashp734@gmail.com>
2025-11-27 22:51:50 +0530

    added-stylings

--- frontend/zatpatreceipe/src/sections/Home/Hero/index.jsx
+++ frontend/zatpatreceipe/src/sections/Home/Hero/index.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import Leaf from '../../../assets/Home/Hero/Leaf.svg'
-import BlueBerry from '../../../assets/Home/Hero/BlueBerry.svg'
-import Donuts from '../../../assets/Home/Hero/Donuts.svg'
-import "../../../styles/Home/Hero/index.css"
+import React from "react";
+import Leaf1 from "../../../assets/Home/Hero/leaf-1.svg";
+import Leaf2 from "../../../assets/Home/Hero/leaf-2.svg";
+import Leaf3 from "../../../assets/Home/Hero/leaf-3.svg";
+import Leaf4 from "../../../assets/Home/Hero/leaf-4.svg";
+import BlueBerry from "../../../assets/Home/Hero/BlueBerry.svg";
+import Donuts from "../../../assets/Home/Hero/Donuts.png";
+import "../../../styles/Home/Hero/index.css";
 function Hero() {
   return (
-    
-      <div className='container'>
-       
-        <div className='donuts'>
-          <img src={Donuts} alt="Donuts" />
-         
+    <div className="hero__container">
+      <h1 className="hero__container-heading">BLUEBERRY</h1>
+
+      <div className="hero__leafCon">
+        <div className="hero__leafBox">
+          <img src = {Leaf1} />
+          <img src = {Leaf2} />
         </div>
-          
-
-       </div>
-
-  )
+        <img src={Donuts} alt="Donuts" className="hero__donut" />
+        <div className="hero__leafBox">
+        <img src = {Leaf3} />
+        <img src = {Leaf4} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
