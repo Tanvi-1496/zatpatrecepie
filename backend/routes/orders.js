592e0bf563c752fd1a4228fe970a8b227e991574
shreyash19-pixel <shreyashp734@gmail.com>
2025-12-20 20:33:22 +0530

    updated-animation-timer

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 
 const router = express.Router()
-const {customerOrders} = require("../controller/orders")
+const {customerOrders} = require( "../controller/orders" )
 
 router.post("/orders", customerOrders)
 
